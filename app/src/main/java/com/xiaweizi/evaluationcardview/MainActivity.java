05042a18ba4f766c384f9a811f5242730dc54579
xiaweizi <1012126908@qq.com>
2018-03-18 10:15:15 +0800

    [dev] 添加测试代码

--- app/src/main/java/com/xiaweizi/evaluationcardview/MainActivity.java
+++ app/src/main/java/com/xiaweizi/evaluationcardview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xiaweizi.evaluationcardview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,13 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void click(View view) {
         EvaluationCardView cardView = new EvaluationCardView(this);
         ArrayList<String> reasonsData = new ArrayList<>();
-        reasonsData.add("1");
-        reasonsData.add("2");
-        reasonsData.add("3");
-        reasonsData.add("4");
+        reasonsData.add("回复太慢");
+        reasonsData.add("对业务不了解");
+        reasonsData.add("服务态度差");
+        reasonsData.add("问题没有得到解决");
         cardView.setReasonsData(reasonsData);
         cardView.show();
         cardView.setOnEvaluationCallback(new EvaluationCardView.OnEvaluationCallback() {
@@ -33,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "starCount:" + starCount + "\n reasons:" + reasons.toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void ratingBar(View view) {
+        startActivity(new Intent(this, EvaluationRatingBarActivity.class));
+    }
+
+    public void reasonLayout(View view) {
+        startActivity(new Intent(this, EvaluationNegReasonLayoutActivity.class));
+    }
+
+    public void cardView(View view) {
+        startActivity(new Intent(this, EvaluationCardViewActivity.class));
     }
 }
